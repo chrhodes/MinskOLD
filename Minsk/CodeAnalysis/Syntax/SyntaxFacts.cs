14bcbb7f1276c69a696e3679cc00f714b61adac9
Christopher Rhodes <chrhodes@cox.net>
2021-08-21 13:46:44 -0700

    Add SyntaxFacts.GetBinaryOperatorKinds() and SyntaxFacts.GetUnaryOperatorKinds()

--- Minsk/CodeAnalysis/Syntax/SyntaxFacts.cs
+++ Minsk/CodeAnalysis/Syntax/SyntaxFacts.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace Minsk.CodeAnalysis.Syntax
 {
@@ -65,6 +66,32 @@ namespace Minsk.CodeAnalysis.Syntax
             }
         }
 
+        public static IEnumerable<SyntaxKind> GetBinaryOperatorKinds()
+        {
+            var kinds = (SyntaxKind[])Enum.GetValues(typeof(SyntaxKind));
+
+            foreach (var kind in kinds)
+            {
+                if (GetBinaryOperatorPrecedence(kind) > 0)
+                {
+                    yield return kind;
+                }
+            }
+        }
+
+        public static IEnumerable<SyntaxKind> GetUnaryOperatorKinds()
+        {
+            var kinds = (SyntaxKind[])Enum.GetValues(typeof(SyntaxKind));
+
+            foreach (var kind in kinds)
+            {
+                if (GetUnaryOperatorPrecedence(kind) > 0)
+                {
+                    yield return kind;
+                }
+            }
+        }
+
         public static string GetText(SyntaxKind kind)
         {
             switch (kind)
@@ -114,7 +141,6 @@ namespace Minsk.CodeAnalysis.Syntax
                 default:
                     return null;
             }
-
         }
     }
 }
