4f17b4784b19f65593e61b257438ee1da4c46dfe
Christopher Rhodes <chrhodes@cox.net>
2021-08-22 09:58:05 -0700

    Add SyntaxNode.Span

--- Minsk/CodeAnalysis/Syntax/SyntaxNode.cs
+++ Minsk/CodeAnalysis/Syntax/SyntaxNode.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 
 namespace Minsk.CodeAnalysis.Syntax
@@ -6,6 +7,20 @@ namespace Minsk.CodeAnalysis.Syntax
     public abstract class SyntaxNode
     {
         public abstract SyntaxKind Kind { get; }
+
+        // NOTE(crhodes)
+        // Not super efficient
+
+        public virtual TextSpan Span
+        {
+            get
+            {
+                var first = GetChildren().First().Span;
+                var last = GetChildren().Last().Span;
+
+                return TextSpan.FromBounds(first.Start, last.End);
+            }
+        }
 
         // NOTE(crhodes)
         // Add notion of children so can walk tree in a generic way
@@ -16,6 +31,11 @@ namespace Minsk.CodeAnalysis.Syntax
         // Does this depend on layout (order) of properties in type???
         // Who call GetChildren
         // Hum.  Yes, it F's up printing of tree
+
+        // Curiously at 1:10:56 in video Episode 5 Immo says he would do abstract and yield like before in production compiler.
+        // So, this seems like fancy, risky stuff
+        // Even if the asserting of order in tests - begs question do we have tests to cover this
+        // Not sure if compiler or reflection guarantees order of properties in classes.
 
 
         public IEnumerable<SyntaxNode> GetChildren()
--- Minsk/CodeAnalysis/TextSpan.cs
+++ Minsk/CodeAnalysis/TextSpan.cs
@@ -14,5 +14,12 @@ namespace Minsk.CodeAnalysis
         public int Start { get; }
         public int Length { get; }
         public int End => Start + Length;
+
+        public static TextSpan FromBounds(int start, int end)
+        {
+            var length = end - start;
+
+            return new TextSpan(start, length);
+        }
     }
 }
