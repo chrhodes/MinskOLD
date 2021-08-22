1d4a1979472c0b120e90e7201c678a1a506b8260
Christopher Rhodes <chrhodes@cox.net>
2021-08-22 13:15:32 -0700

    Add support for multiple lines in REPL

--- mc/Program.cs
+++ mc/Program.cs
@@ -1,10 +1,12 @@
 using Minsk.CodeAnalysis;
 using Minsk.CodeAnalysis.Binding;
 using Minsk.CodeAnalysis.Syntax;
+using Minsk.CodeAnalysis.Text;
 
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading;
 
 using VNC;
@@ -24,33 +26,52 @@ namespace Minsk
 
             bool showTree = true;
             var variables = new Dictionary<VariableSymbol, object>();
+            var textBuilder = new StringBuilder();
 
             while (true)
             {
-                Console.Write("> ");
-
-                var line = Console.ReadLine();
-
-                if (string.IsNullOrWhiteSpace(line))
+                if (textBuilder.Length == 0)
                 {
-                    Log.APPLICATION_START($"Exit - No Input Received", Common.LOG_CATEGORY, startTicks);
-
-                    return;
+                    Console.Write("> ");
+                }
+                else
+                {
+                    Console.Write("| ");
                 }
 
-                if (line == "#showTree")
+                var input = Console.ReadLine();
+                var isBlank = string.IsNullOrWhiteSpace(input);
+
+                if (textBuilder.Length == 0)
                 {
-                    showTree = !showTree;
-                    Console.WriteLine(showTree ? "Showing parse trees." : "Not showing parse trees");
+                    if (isBlank)
+                    {
+                        break;
+                    }
+                    else if (input == "#showTree")
+                    {
+                        showTree = !showTree;
+                        Console.WriteLine(showTree ? "Showing parse trees." : "Not showing parse trees");
+                        continue;
+                    }
+                    else if (input == "#cls")
+                    {
+                        Console.Clear();
+                        continue;
+                    }
+                }
+
+                textBuilder.AppendLine(input);
+                var text = textBuilder.ToString();
+
+                var syntaxTree = SyntaxTree.Parse(text);
+
+                if (! isBlank
+                    && syntaxTree.Diagnostics.Any())
+                {
                     continue;
                 }
-                else if (line == "#cls")
-                {
-                    Console.Clear();
-                    continue;
-                }
 
-                var syntaxTree = SyntaxTree.Parse(line);
                 var compilation = new Compilation(syntaxTree);
                 var result = compilation.Evaluate(variables);
 
@@ -70,22 +91,24 @@ namespace Minsk
                 }
                 else
                 {
-                    var text = syntaxTree.Text;
-
                     foreach (var diagnostic in result.Diagnostics)
                     {
-                        var lineIndex = text.GetLineIndex(diagnostic.Span.Start);
+                        var lineIndex = syntaxTree.Text.GetLineIndex(diagnostic.Span.Start);
                         var lineNumber = lineIndex + 1;
-                        var character = diagnostic.Span.Start - text.Lines[lineIndex].Start + 1;
+                        var line = syntaxTree.Text.Lines[lineIndex];
+                        var character = diagnostic.Span.Start - line.Start + 1;
 
                         Console.WriteLine();
                         Console.ForegroundColor = ConsoleColor.Red;
                         Console.Write($"({lineNumber}, {character}: ");
                         Console.WriteLine(diagnostic);
 
-                        var prefix = line.Substring(0, diagnostic.Span.Start);
-                        var error = line.Substring(diagnostic.Span.Start, diagnostic.Span.Length);
-                        var suffix = line.Substring(diagnostic.Span.End);
+                        var prefixSpan = TextSpan.FromBounds(line.Start, diagnostic.Span.Start);
+                        var suffixSpan = TextSpan.FromBounds(diagnostic.Span.End, line.End);
+
+                        var prefix = syntaxTree.Text.ToString(prefixSpan);
+                        var error = syntaxTree.Text.ToString(diagnostic.Span);
+                        var suffix = syntaxTree.Text.ToString(suffixSpan);
 
                         Console.ResetColor();
                         Console.Write("    ");
@@ -103,10 +126,10 @@ namespace Minsk
                     Console.WriteLine();
                 }
 
+                textBuilder.Clear();
+
                 Log.APPLICATION_START($"Exit", Common.LOG_CATEGORY, startTicks);
             }
         }
-
-
     }
 }
