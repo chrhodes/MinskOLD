00b8c0082659b4a39ab095ead3cfca26715e5ccf
Christopher Rhodes <chrhodes@cox.net>
2021-08-24 18:24:21 -0700

    Add colorization to REPL

--- mc/Program.cs
+++ mc/Program.cs
@@ -30,14 +30,18 @@ namespace Minsk
 
             while (true)
             {
+                Console.ForegroundColor = ConsoleColor.Green;
+
                 if (textBuilder.Length == 0)
                 {
-                    Console.Write("> ");
+                    Console.Write("» ");
                 }
                 else
                 {
-                    Console.Write("| ");
+                    Console.Write("· ");
                 }
+
+                Console.ResetColor();
 
                 var input = Console.ReadLine();
                 var isBlank = string.IsNullOrWhiteSpace(input);
@@ -77,17 +81,20 @@ namespace Minsk
 
                 if (showTree)
                 {
-                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+                    Console.ForegroundColor = ConsoleColor.DarkGray;
 
                     syntaxTree.Root.WriteTo(Console.Out);
-                    //PrettyPrint2(syntaxTree.Root);
 
                     Console.ResetColor();
                 }
 
                 if (!result.Diagnostics.Any())
                 {
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+
                     Console.WriteLine(result.Value);
+
+                    Console.ResetColor();
                 }
                 else
                 {
